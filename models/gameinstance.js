8c4e8860f2947a660597697aacf4f392920fe1c1
Hector <baseballhector@gmail.com>
2023-12-19 19:01:02 -0500

    Game instance schema

---
+++ models/gameinstance.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const GameInstanceSchema = new Schema({
+  game: {
+    type: Schema.Types.ObjectId,
+    ref: "Game",
+    required: true,
+  },
+  console: {
+    type: Schema.Types.ObjectId,
+    ref: "Console",
+    required: true,
+  },
+  price: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  condition: {
+    type: String,
+    required: true,
+    enum: ["New", "Used"],
+  },
+});
+
+// Virtual for game instance's url.
+GameInstanceSchema.virtual("url").get(function () {
+  return `/catalog/gameinstance/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("GameInstance", GameInstanceSchema);
