9845045273cf5d4d76ea5bcaf7c9fa5a25021f11
Hector <baseballhector@gmail.com>
2023-12-20 17:20:20 -0500

    Set up connection to MongoDB

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ var usersRouter = require("./routes/users");
 
 var app = express();
 
+// Set up a Mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+const dev_db_url = "placeholder";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+// Wait for database connect. Logs error if fail to connect.
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
