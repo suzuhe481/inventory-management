0a7d330f5b0d1f0988af9f93b98491ba6331be3d
Hector <baseballhector@gmail.com>
2023-12-24 18:05:49 -0500

    GET and POST routes for creating game instance form

--- controllers/gameinstanceController.js
+++ controllers/gameinstanceController.js
@@ -1,5 +1,9 @@
 const GameInstance = require("../models/gameinstance");
+const Game = require("../models/game");
+const Console = require("../models/console");
+
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 // Displays a list of all GamesInstances.
 exports.gameinstance_list = asyncHandler(async (req, res, next) => {
@@ -34,13 +38,67 @@ exports.gameinstance_detail = asyncHandler(async (req, res, next) => {
 
 // Displays GameInstance create form on GET.
 exports.gameinstance_create_get = asyncHandler(async (req, res, next) => {
-  res.send("Not implemented: GameInstance create GET");
+  const allGames = await Game.find({}, "title")
+    .populate("consoles_available")
+    .sort({ title: 1 })
+    .exec();
+
+  res.render("gameinstance/form", {
+    title: "Create Game Instance (copy)",
+    game_list: allGames,
+  });
 });
 
 // Handles GameInstance create on POST.
-exports.gameinstance_create_post = asyncHandler(async (req, res, next) => {
-  res.send("Not implemented: GameInstance create POST");
-});
+exports.gameinstance_create_post = [
+  // Validate form data
+  body("game", "Game must be specified").trim().isLength({ min: 1 }).escape(),
+  body("console", "Console must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("price", "Price must be greater than 0.01")
+    .trim()
+    .isFloat({ min: 0.01 })
+    .escape(),
+  body("condition", "Invalid condition").escape(),
+
+  // Async
+  asyncHandler(async (req, res, next) => {
+    // Extract errors
+    const errors = validationResult(req);
+
+    // Create Game Instance object
+    const gameInstance = new GameInstance({
+      game: req.body.game,
+      console: req.body.console,
+      price: req.body.price,
+      condition: req.body.condition,
+    });
+
+    // Return form again if errors exist.
+    if (!errors.isEmpty()) {
+      const allGames = await Game.find({}, "title")
+        .populate("consoles_available")
+        .sort({ title: 1 })
+        .exec();
+
+      res.render("gameinstance/form", {
+        title: "Create Game Instance",
+        game_list: allGames,
+        errors: errors.array(),
+        gameinstance: gameInstance,
+      });
+      return;
+    }
+    // Form data is valid. Save object and redirect to it's page.
+    else {
+      await gameInstance.save();
+
+      res.redirect(gameInstance.url);
+    }
+  }),
+];
 
 // Displays GameInstance delete form on GET.
 exports.gameinstance_delete_get = asyncHandler(async (req, res, next) => {
